ba5fd759b4b2bee600fad0e52d89967a039f850a
Aaquib111 <aaquibahm@gmail.com>
2025-12-05 22:45:59 -0800

    gemini

--- nanojax.py
+++ nanojax.py
@@ -7,3 +7,15 @@ f = lambda x: nj.sin(nj.pow(x, 2))
 df_dx = grad(f)
 df2_d2x = grad(df_dx)
 """
+
+import os
+from google import genai
+
+client = genai.Client(api_key=os.environ['GEMINI_API_KEY'])
+
+response = client.models.generate_content(
+    model="gemini-3-pro-preview",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
